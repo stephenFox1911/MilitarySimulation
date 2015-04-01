68193454b3a5ed02f423448a76350b7b8b3034be
steve <steve@steve-HP.(none)>
2015-04-01 12:56:57 -0400

    added machinegunner and fireteam leader

---
+++ usMachineGunner.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from soldier import Soldier
+
+class USmachineGunner(Soldier):
+    'USrifleman with SAW'
+
+    def __init__(self, name, team, fireteam, posx, posy, orientation, aggression):
+        # subclass specific arguments go here
+        Soldier.__init__(self, name, team, fireteam, posx, posy, orientation, aggression)
+        
+
+    def observe(self):
+        Soldier.observe(self)
+
+    def decide(self):
+        Soldier.decide(self)
+    
+    def act(self):
+        Soldier.act(self)
+
+    def displaySoldier(self):
+        Soldier.displaySoldier(self)
+
+
