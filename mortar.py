ec7be99778b18b6388c741f82148086254d97eac
Wayne Lee <wayne923@gmail.com>
2015-04-01 17:50:21 -0400

    added mortar

--- mortar.py
+++ mortar.py
@@ -8,16 +8,29 @@ class Mortar(object):
         self.currentAmmo = maxAmmo
 
     def attack(self, posx, posy):
-        #generate an amount of aiming error for x and y
 
-        #calculate a kill, heavy suppression, and weak suppression radius
+        #Adding error to intial landing position, which is posx and posy
+        landingX = posx + randint(-10, 10)
+        landingY = posy + randint(-10, 10)
 
-        #find which soldiers were affected
+        #Actual radius of mortar damage; r1 = dead; r2 = heavy; r3 = light
+        r1 = 5
+        r2 = 15
+        r3 = 50
+
+        heavySuppression = 90
+        lightSuppression = 45
+
+        #Looking for solider to hit
         for soldier in Soldier.soldiers:
-            #use soldier.posx and soldier.posy to get it's position
-            #update soldier.suppression when necessary
-            #I haven't figured out how to represent death yet. 
+            if soldier.posx < landingX + r1 and soldier.posx > landingX - r1 and soldier.posy < landingY + r1 and soldier.posy > landingY - r1:
+                soldier.isDead = True
+            if soldier.posx < landingX + r2 and soldier.posx > landingX - r2 and soldier.posy < landingY + r2 and soldier.posy > landingY - r2:
+                soldier.suppression += 5/soldier.posx * heavySuppression
+            if soldier.posx < landingX + r3 and soldier.posx > landingX - r3 and soldier.posy < landingY + r3 and soldier.posy > landingY - r3:
+                soldier.suppression +=  5/soldier.posx * lightSuppression
+            else:
+                soldier.suppression = soldier.suppression
 
+        #reduce ammo
         self.currentAmmo -= 1
-        
-        
