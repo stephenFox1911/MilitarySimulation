c396294a533eac426d9d67541d523aef6c77e43b
Adam Johnson <acjohnson.ruiz@gmail.com>
2015-04-07 13:02:03 -0400

    Updated cover class to be only one pixel

--- cover.py
+++ cover.py
@@ -1,34 +1,30 @@
 import math
 
 class Cover:
-    def __init__(self, nw_x, nw_y, se_x, se_y, quality, occupancy, direction, passable=True, fireable=True):
+    def __init__(self, x, y, quality, occupancy, direction='-', passable=True, fireable=True):
         '''
         @summary: a class to represent cover
-        @param nw_x: the x-coord for the northwest corner of cover
-        @param nw_y: the y-coord for the northwest corner of cover
-        @param se_x: the x-coord for the southeast corner of cover
-        @param se_y: the y-coord for the southeast corner of cover
-        @param quality: int, ranges from 0 to 2, represents quality of cover
+        @param posx: the x-coord for the cover
+        @param posy: the y-coord for the cover
+        @param quality: int, ranges from 0 to 1, represents quality of cover
         @param occupancy: maximum occupancy
         @param direction: direction in which the cover faces. '-' means east to west, '|' means north to south, '/' means southwest to northeast, '\\' means northwest to southeast
         @param passable: Boolean, determines if soldier can pass though (or over) the cover (defaults to true)
         @param fireable: Boolean, determines if the soldier can fire from cover (defaults to true)
         '''
-        self.nw_x = nw_x
-        self.nw_y = nw_y
-        self.se_x = se_x
-        self.se_y = se_y
+        self.posx = x
+        self.posy = y
         self.quality = quality
         self.occupancy = occupancy
         self.current_occupancy = 0
         self.passable = passable
         self.fireable = fireable
-        self.center = ((nw_x+se_x)/2, (nw_y+se_y)/2)
+        self.center = (x,y) #legacy code
     def in_cover(self,x,y):
         '''
         @summary: takes in the soldier's x,y corrdinates to determine if he is in this particular cover
         '''
-        return x >= self.nw_x and x <= self.se_x and y <= self.nw_y and self.y >= se_y
+        return x == self.posx and y == self.posy
     def cover_available(self):
         return self.current_occupancy < self.occupancy
     def add_to_cover(self):
